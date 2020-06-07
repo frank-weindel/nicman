5bab91aa0595167a094e3fb26fabedf66fcd5dce
Frank Weindel <foxtrotwhiskey9@gmail.com>
2020-06-07 02:42:56 -0400

    Simplify project for tutorial

--- src/game.js
+++ src/game.js
@@ -1,7 +1,5 @@
-import Phaser, { Game } from 'phaser';
+import Phaser from 'phaser';
 import assets from './assets/*.png';
-
-let platforms, player, stars, score = 0, scoreText, bombs, gameOver;
 
 const WIDTH = 800;
 const HEIGHT = 608;
@@ -12,46 +10,24 @@ const THICKNESS = 32;
 const levelMap = [
     "=========================",
     "=                       =",
+    "= =  =  =  ==  =  =  =  =",
+    "= =  =  =  ==  =  =  =  =",
+    "= =  =  =  ==  =  =  =  =",
+    "= =  =  =  ==  =  =  =  =",
     "=                       =",
     "=                       =",
+    "= = ===== ===== ===== = =",
+    "= ===   ===   ===   === =",
     "=                       =",
     "=                       =",
     "= ===================== =",
+    "=           o           =",
     "=                       =",
     "= ===================== =",
-    "=                       =",
-    "= ===================== =",
-    "=                       =",
-    "= ===================== =",
-    "=...........o...........=",
-    "=.=====================.=",
-    "=.......................=",
-    "=.=====================.=",
-    "=*.....................*=",
+    "= =   =   =   =   =   = =",
+    "=   =   =   =   =   =   =",
     "=========================",
 ];
-
-const blankLevelMap = [
-    "=========================",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=                       =",
-    "=========================",
-]
 
 class GameScene extends Phaser.Scene {
     constructor() {
@@ -65,90 +41,34 @@ class GameScene extends Phaser.Scene {
     update() {
         let cursors = this.input.keyboard.createCursorKeys();
 
-        let playerAngle = this.playerSprite.rotation;
 
-        if (this.justTurned) {
-            if (this.direction === 'left') {
-                if (this.player.body.touching.left) {
-                    this.player.setX(Math.round((this.player.x - 16) / THICKNESS) * THICKNESS + 16);
-                    this.player.setY(Math.round((this.player.y - 16) / THICKNESS) * THICKNESS + 16);
-                }
-                this.player.body.setVelocity(-160, 0);
-            }
-            else if (this.direction === 'right') {
-                if (this.player.body.touching.right) {
-                    this.player.setX(Math.round((this.player.x - 16) / THICKNESS) * THICKNESS + 16);
-                    this.player.setY(Math.round((this.player.y - 16) / THICKNESS) * THICKNESS + 16);
-                }
-                this.player.body.setVelocity(160, 0);
-            }
-            else if (this.direction === 'up') {
-                if (this.player.body.touching.up) {
-                    this.player.setX(Math.round((this.player.x - 16) / THICKNESS) * THICKNESS + 16);
-                    this.player.setY(Math.round((this.player.y - 16) / THICKNESS) * THICKNESS + 16);
-                }
-                this.player.body.setVelocity(0, -160);
-            }
-            else if (this.direction === 'down') {
-                if (this.player.body.touching.down) {
-                    this.player.setX(Math.round((this.player.x - 16) / THICKNESS) * THICKNESS + 16);
-                    this.player.setY(Math.round((this.player.y - 16) / THICKNESS) * THICKNESS + 16);
-                }
-                this.player.body.setVelocity(0, 160);
-            }
-            this.justTurned = false;
-        }
-
-        if (this.direction !== 'left' && cursors.left.isDown && !this.physics.overlap(this.haloLeft, this.level)) {
+        if (cursors.left.isDown) {
             this.player.body.setVelocity(-160, 0);
-            this.playerSprite.angle = 180;
-            this.justTurned = true;
-            this.direction = 'left';
+            this.player.angle = 180;
             this.lastTurnPos = `x: ${this.player.x}, y: ${this.player.y}`
         }
-        else if (this.direction !== 'right' && cursors.right.isDown && !this.physics.overlap(this.haloRight, this.level)) {
+        else if (cursors.right.isDown) {
             this.player.body.setVelocity(160, 0);
-            this.playerSprite.angle = 0;
-            this.justTurned = true;
-            this.direction = 'right';
+            this.player.angle = 0;
             this.lastTurnPos = `x: ${this.player.x}, y: ${this.player.y}`
         }
-        else if (this.direction !== 'up' && cursors.up.isDown && !this.physics.overlap(this.haloTop, this.level)) {
+        else if (cursors.up.isDown) {
             this.player.body.setVelocity(0, -160);
-            this.playerSprite.angle = 270;
-            this.justTurned = true;
-            this.direction = 'up';
+            this.player.angle = 270;
             this.lastTurnPos = `x: ${this.player.x}, y: ${this.player.y}`
         }
-        else if (this.direction !== 'down' && cursors.down.isDown && !this.physics.overlap(this.haloBottom, this.level)) {
+        else if (cursors.down.isDown) {
             this.player.body.setVelocity(0, 160);
-            this.playerSprite.angle = 90;
-            this.justTurned = true;
-            this.direction = 'down';
+            this.player.angle = 90;
             this.lastTurnPos = `x: ${this.player.x}, y: ${this.player.y}`
-            // this.tweens.add({
-            //     targets: this.playerSprite,
-            //     duration: 200,
-            //     rotation: Phaser.Math.Angle.RotateTo(playerAngle, Math.PI/2, 1)
-            // });
         }
 
         this.posText.setText(
-            `cur rounded - x: ${Math.round((this.player.x - 16) / THICKNESS) * THICKNESS + 16}, y: ${Math.round((this.player.y - 16) / THICKNESS) * THICKNESS + 16}\n` +
-            `cur pos     - x: ${this.player.x}, y: ${this.player.y}\n` +
-            `last turn   - ${this.lastTurnPos || ''}`
+            `cur pos     - x: ${Math.round(this.player.x)}, y: ${Math.round(this.player.y)}`
         );
-
-
-
-
     }
 
     preload() {
-        this.load.image('sky', assets.sky);
-        this.load.image('ground', assets.platform);
-        this.load.image('star', assets.star);
-        this.load.image('bomb', assets.bomb);
         this.load.spritesheet('nicman',
             assets.nicman,
             { frameWidth: 32, frameHeight: 32 }
@@ -178,34 +98,10 @@ class GameScene extends Phaser.Scene {
             })
         });
 
-        // // Border
-        // level.add(this.add.rectangle(0, 0, WIDTH, THICKNESS, 0x00aa00).setOrigin(0, 0));
-        // level.add(this.add.rectangle(WIDTH - THICKNESS, 0, THICKNESS, HEIGHT, 0x00aa00).setOrigin(0, 0));
-        // level.add(this.add.rectangle(0, HEIGHT - THICKNESS, WIDTH, THICKNESS, 0x00aa00).setOrigin(0, 0));
-        // level.add(this.add.rectangle(0, 0, THICKNESS, HEIGHT, 0x00aa00).setOrigin(0, 0));
-
-        // // Inneards
-        // level.add(this.add.rectangle(0, THICKNESS+32, WIDTH-THICKNESS-32, THICKNESS, 0x00aa00).setOrigin(0, 0));
-        // level.add(this.add.rectangle(THICKNESS+32, THICKNESS*2+32+32+1, WIDTH-THICKNESS-32, THICKNESS, 0x00aa00).setOrigin(0, 0));
-
-
-        const SHOW_HALO = false;
-        this.playerSprite = this.add.sprite(0, 0, 'nicman');
-        this.haloTop = this.add.rectangle(0, -17, 32, 1, 0x0000ff).setAlpha(SHOW_HALO ? 1 : 0);
-        this.haloRight = this.add.rectangle(17, 0, 1, 32, 0x0000ff).setAlpha(SHOW_HALO ? 1 : 0);
-        this.haloBottom = this.add.rectangle(0, 17, 32, 1, 0x0000ff).setAlpha(SHOW_HALO ? 1 : 0);
-        this.haloLeft = this.add.rectangle(-17, 0, 1, 32, 0x0000ff).setAlpha(SHOW_HALO ? 1 : 0);
-        this.player = this.add.container(playerLevelPos.x * THICKNESS + 16, playerLevelPos.y * THICKNESS + 16, [
-            this.haloTop,
-            this.haloRight,
-            this.haloBottom,
-            this.haloLeft,
-            this.playerSprite
-        ]);
+        this.player = this.add.sprite(playerLevelPos.x * THICKNESS + 16, playerLevelPos.y * THICKNESS + 16, 'nicman');
         this.player.setSize(32, 32);
-        // this.player.
+
         this.physics.world.enable(this.player);
-        this.physics.world.enable([this.haloTop, this.haloRight, this.haloBottom, this.haloLeft]);
 
         this.physics.add.collider(this.player, level);
 
@@ -220,102 +116,10 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         });
 
-        this.playerSprite.anims.play('right');
+        this.player.anims.play('right');
 
-        this.posText = this.add.text(16, 16, 'Score: 0', { fontSize: '20px', fill: '#fff' });
-
-        // level.add.rectangle(0, HEIGHT - THICKNESS, WIDTH, THICKNESS, 0x00aa00).setOrigin(0, 0);
-        // level.add.rectangle(0, HEIGHT - THICKNESS, WIDTH, THICKNESS, 0x00aa00).setOrigin(0, 0);
-        // level.add.rectangle(0, HEIGHT - THICKNESS, WIDTH, THICKNESS, 0x00aa00).setOrigin(0, 0);
-        // this.add.image(400, 300, 'sky');
-
-        // platforms = this.physics.add.staticGroup();
-
-        // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        // platforms.create(600, 400, 'ground');
-        // platforms.create(50, 250, 'ground');
-        // platforms.create(750, 220, 'ground');
-
-
-        // player.setBounce(0.2);
-        // player.setCollideWorldBounds(true);
-        // player.body.setGravityY(300)
-
-        // this.physics.add.collider(player, platforms);
-
-        // this.anims.create({
-        //     key: 'left',
-        //     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
-
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [ { key: 'dude', frame: 4 }],
-        //     frameRate: 20
-        // });
-
-        // this.anims.create({
-        //     key: 'right',
-        //     frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
-
-        // stars = this.physics.add.group({
-        //     key: 'star',
-        //     repeat: 11,
-        //     setXY: { x: 12, y: 0, stepX: 70 }
-        // });
-
-        // stars.children.iterate(child => {
-        //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-        // });
-
-        // this.physics.add.collider(stars, platforms);
-
-        // this.physics.add.overlap(player, stars, this.collectStar, null, this);
-
-        // bombs = this.physics.add.group();
-
-        // this.physics.add.collider(bombs, platforms);
-
-        // this.physics.add.collider(player, bombs, this.hitBomb, null, this);
-
-        // scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+        this.posText = this.add.text(16, 16, '', { fontSize: '20px', fill: '#fff' });
     }
-
-    // hitBomb(player, bomb) {
-    //     this.physics.pause();
-
-    //     player.setTint(0xff0000);
-
-    //     player.anims.play('turn');
-
-    //     gameOver = true;
-    // }
-
-    // collectStar(player, star) {
-    //     star.disableBody(true, true);
-
-    //     score += 10;
-    //     scoreText.setText('Score: ' + score);
-
-    //     if (stars.countActive(true) === 0) {
-    //         stars.children.iterate(child => {
-    //             child.enableBody(true, child.x, 0, true, true);
-    //         });
-
-    //         let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-    //         let bomb = bombs.create(x, 16, 'bomb');
-    //         bomb.setBounce(1);
-    //         bomb.setCollideWorldBounds(true);
-    //         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    //     }
-    // }
 }
 
 let config = {
